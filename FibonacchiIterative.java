a20e32f0646643d046527185b6aa543d24627197
dhanvanth022 <dhanvanthadari8@gmail.com>
2026-06-29 10:13:27 +0530

    FibonacchiIterative

---
+++ FibonacchiIterative.java
@@ -0,0 +1,18 @@
+
+public class FibonacchiIterative {
+    public static void PrintFibonacchi(int n){
+        int a = 0, b =1;
+
+        for(int i = 0; i < n;i++){
+            System.out.print(a+" ");
+            int c = a+b;
+            a = b;
+            b = c;
+        }
+    }
+    public static void main(String[] args) {
+        int n = 10;
+
+        PrintFibonacchi(n);
+    }
+}
